e3caa1e9637a4438cb7b28238195fe8028760a62
CookieTonks <91743424+CookieTonks@users.noreply.github.com>
2023-07-16 13:35:58 -0600

    fix: produccion expoprt

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Maatwebsite\Excel\Facades\Excel;
-use App\Exports\Produccion;
+use App\Exports\produccion;
 use App\Exports\embarques;
 use App\Exports\ordenes;
 use App\Exports\calidad;
@@ -18,7 +18,7 @@ class ExportController extends Controller
 {
     public function exportar_produccion()
     {
-        return Excel::download(new Produccion, 'production.xlsx');
+        return Excel::download(new produccion, 'production.xlsx');
     }
 
     public function exportar_ordenes()
